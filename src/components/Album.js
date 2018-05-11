00cecff3e997e72357e183b5b1e07cf1e2bd01af
Jose Mena <josemena0114@gmail.com>
2018-05-11 12:30:55 -0400

    player-bar previous button works

--- src/components/Album.js
+++ src/components/Album.js
@@ -45,6 +45,14 @@ class Album extends Component {
         }
     }
     
+    handlePrevClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.max(0, currentIndex - 1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
+    }
+    
     
     render() {
         return (
@@ -82,7 +90,12 @@ class Album extends Component {
                 )}
                 </tbody>
                 </table>
-                <PlayerBar isPlaying={this.state.isPlaying} currentSong={this.state.currentSong}/>
+                <PlayerBar
+                isPlaying={this.state.isPlaying}
+                currentSong={this.state.currentSong}
+                handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+                handlePrevClick={() => this.handlePrevClick()}
+                />
                 </section>
                 );
     }
